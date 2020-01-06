21b8faccd604550c58f1eb9cd1767a62492df015
Luděk Rous <rousludek@gmail.com>
2020-01-06 22:13:55 +0100

    try to create custom bootstrap styles

--- apollo-ng/src/app/admin/pages/activities/activities-pages.service.ts
+++ apollo-ng/src/app/admin/pages/activities/activities-pages.service.ts
@@ -1,5 +1,7 @@
 import { Subject } from 'rxjs';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class ActivitiesPagesService {
     public addActivityResultSource = new Subject();
     public addActivityResult$ = this.addActivityResultSource.asObservable();
